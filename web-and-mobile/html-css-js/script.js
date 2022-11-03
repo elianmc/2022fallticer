bb122ab33f6e37835241b31390bbc98c76000982
elian caballero <eliancaballero3a@gmail.com>
2022-11-03 00:02:53 -0500

    web

---
+++ web-and-mobile/html-css-js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", function ()
+{
+    //get click elements.
+    let clickButtonElement = document.getElementById("click_button");
+    let clickCounterElement = document.getElementById("click_counter");
+    
+    //counter value.
+    let counter =0;
+
+    //button click function
+    let clickButtonFunction = function ()
+    {
+        counter++;
+
+        clickCounterElement.innerHTML = counter;
+    };
+    //attach click function to button.
+    clickButtonElement.addEventListener("click", clickButtonFunction);
+});
