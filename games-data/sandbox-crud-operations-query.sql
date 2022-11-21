aaddc6477460bd2a19ccf5834d6581722f8cfc0f
elian caballero <eliancaballero3a@gmail.com>
2022-11-21 14:23:21 -0600

    Add games fata project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int32_value,bool_value,double_value) VALUES (444,0,111.111);
+
+--UPDATE sandbox SET int64_value = 234 WHERE id = 55;
+
+--DELETE FROM sandbox WHERE id = 58;
